8f19961bb2b83c3dc1213f2954e81e91c270f973
Rishin Goswami <rishin.goswami@gmail.com>
2022-07-04 01:14:06 +0530

    Refactor step 1 of very big refactoring

---
+++ archie.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Initial Boilerplate - See https://github.com/xwmx/bash-boilerplate/blob/master/bash-simple for details
+###############################################################################
+
+set -o nounset
+set -o errexit
+trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
+set -o errtrace
+set -o pipefail
+#IFS=$'\n\t' # TODO: why doesn't this work?
+_ME="$(basename "${0}")"
+
+
+# Start of script
+###############################################################################
+if [[ "${1:--h}" =~ ^-h|--help$ ]]; then
+  mvn exec:java -Dexec.mainClass="tinkerbeast.App" -Dexec.args="--help"  
+else
+  mvn exec:java -Dexec.mainClass="tinkerbeast.App" -Dexec.args="${*}"
+fi
+
+
+
+
+
+
+
+
